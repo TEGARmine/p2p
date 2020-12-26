a3606fa1aa21d5b8eb09a768797391d44216d9a7
tegar pratama <tegarp00@gmail.com>
2020-12-26 22:15:37 +0700

    Service EmailChek

---
+++ handler/user.go
@@ -0,0 +1,98 @@
+package handler
+
+import (
+	"net/http"
+	"sadlyy/helper"
+	"sadlyy/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct di atas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// token, err := h.jwtService.GenerateToken()
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	// user memasukkan input (email & password)
+	// input ditangkap handler
+	// mapping dari input user ke input struct
+	// input struct passing service
+	// di service mencari dgn bantuan repository user dengan email x
+	// mencocokkan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfuly loggedin", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
+}
+
+func CheckEmailAvailability(c *gin.Context) {
+	// ada input email dari user
+	// input email di-mapping ke struct input
+	// struct input di passing ke service
+	// service akan manggil repository - email sudah ada atau belum
+	// repository - db
+
+}
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"sadlyy/handler"
+	"sadlyy/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/sdly?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	//input := user.LoginInput{
+	//	Email: "tegarp00@gmail.com",
+	//	Password: "password",
+	//}
+
+	//user, err := userService.Login(input)
+	//if err != nil {
+	//	fmt.Println("terjadi kesalahan")
+	//	fmt.Println(err.Error())
+	//}
+
+	//fmt.Println(user.Email)
+	//fmt.Println(user.Name)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
+
+	router.Run()
+
+	// input dari user
+	// handler, mapping input dari user -> struct input
+	// service : melakukan mapping dari struct input ke struct User
+	// repository
+	// db
+}
